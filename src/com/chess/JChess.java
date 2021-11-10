0436740481a7029bccf08f7fb23282164cfcb2ac
a_Chav13 <anuj.chavan3101@gmail.com>
2021-11-10 01:04:49 -0800

    Art added, Pieces and Board drawn

--- src/com/chess/JChess.java
+++ src/com/chess/JChess.java
@@ -1,6 +1,7 @@
 package com.chess;
 
 import com.chess.engine.board.*;
+import com.chess.engine.pieces.Pawn;
 import com.chess.gui.Table;
 
 public class JChess {
@@ -8,6 +9,16 @@ public class JChess {
         Board board = Board.createStandardBoard();
 
         System.out.println(board);
+
+//        Pawn e2 = (Pawn) board.getTile(52).getPiece();
+//        Move.PawnJump e2_e4 = new Move.PawnJump(board, e2, 36);
+//        board = e2_e4.execute();
+//        System.out.println(board);
+//
+//        Pawn e7 = (Pawn) board.getTile(12).getPiece();
+//        Move.PawnMove e7_e5 = new Move.PawnMove(board, e7, 28);
+//        board = e7_e5.execute();
+//        System.out.println(board);
 
         Table table = new Table();
     }
--- src/com/chess/gui/Table.java
+++ src/com/chess/gui/Table.java
@@ -1,26 +1,54 @@
 package com.chess.gui;
 
+import com.chess.engine.board.Board;
+import com.chess.engine.board.BoardUtils;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Table {
+
     private final JFrame gameFrame;
-    private static Dimension OUTER_FRAME_DIMENSION = new Dimension(600,600);
+    private final BoardPanel boardPanel;
+    private final Board chessBoard;
+
+    private final static Dimension OUTER_FRAME_DIMENSION = new Dimension(600,600);
+    private static final Dimension BOARD_PANEL_DIMENSION = new Dimension(400, 350);
+    private static final Dimension TILE_PANEL_DIMENSION = new Dimension(10,10);
+
+    private static final String defaultPieceImagesPath = "art/lichess/";
+
+    private final Color lightTileColor = Color.decode("#FFFACD");
+    private final Color darkTileColor = Color.decode("#593E1A");
 
     public Table(){
         this.gameFrame = new JFrame("JChess");
+        this.gameFrame.setLayout(new BorderLayout());
 
-        final JMenuBar tableMenuBar = new JMenuBar();
-        populateMenuBar(tableMenuBar);
+        final JMenuBar tableMenuBar = createTableMenuBar();
         this.gameFrame.setJMenuBar(tableMenuBar);
         this.gameFrame.setSize(OUTER_FRAME_DIMENSION);
+
+        this.chessBoard = Board.createStandardBoard();
+        this.boardPanel = new BoardPanel();
+        this.gameFrame.add(this.boardPanel, BorderLayout.CENTER);
+
         this.gameFrame.setVisible(true);
+
     }
 
-    private void populateMenuBar(final JMenuBar tableMenuBar) {
+    private JMenuBar createTableMenuBar() {
+        final JMenuBar tableMenuBar = new JMenuBar();
         tableMenuBar.add(createFileMenu());
+        return tableMenuBar;
     }
 
     private JMenu createFileMenu() {
@@ -34,6 +62,73 @@ public class Table {
             }
         });
         fileMenu.add(openPGN);
+
+        final JMenuItem exitMenuItem = new JMenuItem("Exit");
+        exitMenuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        fileMenu.add(exitMenuItem);
+
+
         return fileMenu;
     }
+
+    private class BoardPanel extends JPanel{
+        final List<TilePanel> boardTiles;
+
+        BoardPanel(){
+            super(new GridLayout(8,8));
+            this.boardTiles = new ArrayList<>();
+            for(int i=0;i< BoardUtils.NUM_TILES;i++){
+                final TilePanel tilePanel = new TilePanel(this, i);
+                this.boardTiles.add(tilePanel);
+                add(tilePanel);
+            }
+
+            setPreferredSize(BOARD_PANEL_DIMENSION);
+            validate();
+        }
+    }
+
+    private class TilePanel extends JPanel{
+        //Maps to a tile
+        private final int tileId;
+
+        TilePanel(final BoardPanel boardPanel, final int tileId){
+            super(new GridBagLayout());
+            this.tileId=tileId;
+            setPreferredSize(TILE_PANEL_DIMENSION);
+            assignTileColor();
+            assignTilePieceIcon(chessBoard);
+            validate();
+        }
+
+        private void assignTilePieceIcon(final Board board){
+            this.removeAll();
+            if(board.getTile(this.tileId).isTileOccupied()){
+                try {
+                    final BufferedImage image = ImageIO.read(new File(defaultPieceImagesPath +
+                            board.getTile(this.tileId).getPiece().getPieceAlliance().toString().substring(0,1) +
+                            board.getTile(this.tileId).getPiece().toString() + ".png"));
+                    add(new JLabel(new ImageIcon(image)));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        private void assignTileColor() {
+            if(BoardUtils.EIGHT_RANK[this.tileId] || BoardUtils.SIXTH_RANK[this.tileId] ||
+                BoardUtils.FOURTH_RANK[this.tileId] || BoardUtils.SECOND_RANK[this.tileId]){
+                setBackground(this.tileId % 2==0 ? lightTileColor : darkTileColor);
+            } else if(BoardUtils.SEVENTH_RANK[this.tileId] || BoardUtils.FIFTH_RANK[this.tileId] ||
+                    BoardUtils.THIRD_RANK[this.tileId] || BoardUtils.FIRST_RANK[this.tileId]) {
+                setBackground(this.tileId % 2 != 0 ? lightTileColor : darkTileColor);
+            }
+        }
+    }
+
 }
